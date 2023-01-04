2841805a4ff48eafe14bd414a8aaa621535e34fd
pacristomeli <ext_pacristo@mercadolibre.com>
2023-01-04 16:30:58 -0300

    lots of things

--- 01-std-lib/concurrency/02-channels/02-range/range.go
+++ 01-std-lib/concurrency/02-channels/02-range/range.go
@@ -6,7 +6,8 @@ func main() {
 	ch := make(chan int, 6)
 
 	go func() {
-		// when the goroutine has sent all the values, it needs to close the channel or there will be a deadlock
+		// when the goroutine has sent all the values,
+		// it needs to close the channel or there will be a deadlock
 		defer close(ch)
 		for i := 0; i < 6; i++ {
 			// send iterator over channel
